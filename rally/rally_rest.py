89b3eae0f64ea2cbf47ef69d50475bfd070e10b1
Olexandr Deviatov <sasca9719@gmail.com>
2022-06-28 12:09:54 +0300

    added directories for toggle and rally

---
+++ rally/rally_rest.py
@@ -0,0 +1,3 @@
+host = 'https://rally1.rallydev.com/slm/webservice/v2.0'
+
+rally_artifacts_route = '/artifact'
---
+++ toggle/toggle_rest.py
@@ -0,0 +1,3 @@
+toggle_host = 'https://api.track.toggl.com/api/v8'
+
+toggle_time_entities_route = '/time_entries'
