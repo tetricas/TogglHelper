aa485ad55f69a699b4abdc1e2f19a8b91fec2a5c
Olexandr Deviatov <sasca9719@gmail.com>
2022-06-28 12:28:27 +0300

    added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")" || echo "cd error"
+
+FILE=config.py
+if [ ! -f "$FILE" ]; then
+   touch "$FILE"
+   echo "username_toggle = ''
+password_toggle = ''
+
+username_rally = ''
+api_key_rally = ''" > "$FILE"
+fi
