89bc4bdbbf4b107fff265a6305b3f65acf4299f6
Olexandr Deviatov <sasca9719@gmail.com>
2022-06-28 12:10:42 +0300

    added rally tasks querying + refactoring

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 from toggl.TogglPy import Toggl
+import requests
 
 import config
-import time_entities
+from rally import rally_rest
+from toggle import time_entities, toggle_rest
 
 
-def createNewTimeEntity(days, functor):
+def create_new_time_entity(days, functor):
+    toggl = Toggl()
+    toggl.setAuthCredentials(config.username_toggle, config.password_toggle)
+
     for day in days:
         if day < 10:
             day = "0" + str(day)
 
         data = functor(day)
-        toggl.postRequest("https://api.track.toggl.com/api/v8/time_entries", parameters=data)
+        url = toggle_rest.toggle_host + toggle_rest.toggle_time_entities_route
+        toggl.postRequest(url, parameters=data)
+
+
+def query_rally_tasks():
+    query = f'((CreatedBy = {config.username_rally}) AND \
+                ((LastUpdateDate >= 2022-06-01) AND (LastUpdateDate <= today)))'
+    query = '?query=' + query
+
+    url = rally_rest.host + rally_rest.rally_artifacts_route + query
+    return requests.get(url, headers={'ZSESSIONID': config.api_key_rally})
 
 
 if __name__ == '__main__':
-    toggl = Toggl()
-    toggl.setAuthCredentials(config.username, config.password)
+    daily = False
+    english = False
+    rally = False
 
-    createNewTimeEntity(range(23, 24), time_entities.createDaily)
+    if daily:
+        create_new_time_entity(range(23, 24), time_entities.createDaily)
+    if english:
+        create_new_time_entity([1], time_entities.createEnglish)
+    if rally:
+        response = query_rally_tasks()
+        print(response.text)
