78c2c4480b90e08006b5ab366e63a2bafdc7fbbf
Olexandr Deviatov <sasca9719@gmail.com>
2022-08-05 19:17:37 +0300

    added automatic days generation

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from toggl.TogglPy import Toggl
 import config
 from rally import rally_rest
 from toggle import time_entities, toggle_rest
+from utils.dates import get_this_month
 
 
-def create_new_time_entity(days, functor):
+def create_new_time_entities(is_daily, is_english):
     toggl = Toggl()
     toggl.setAuthCredentials(config.username_toggle, config.password_toggle)
 
-    for day in days:
-        if day < 10:
-            day = "0" + str(day)
+    for date, weekday in get_this_month():
+        if weekday in range(0, 4) and is_daily:
+            functor = time_entities.create_daily
+        elif weekday == 4 and is_english:
+            functor = time_entities.create_english
+        else:
+            raise Exception("Wrong weekday")
 
-        data = functor(day)
+        data = functor(date)
         url = toggle_rest.toggle_host + toggle_rest.toggle_time_entities_route
         toggl.postRequest(url, parameters=data)
 
@@ -29,14 +34,11 @@ def query_rally_tasks():
 
 
 if __name__ == '__main__':
-    daily = False
-    english = False
-    rally = False
+    is_daily = False
+    is_english = False
+    is_rally = False
 
-    if daily:
-        create_new_time_entity(range(23, 24), time_entities.create_daily)
-    if english:
-        create_new_time_entity([1], time_entities.create_english)
-    if rally:
+    create_new_time_entities(is_daily, is_english)
+    if is_rally:
         response = query_rally_tasks()
         print(response.text)
---
+++ utils/dates.py
@@ -0,0 +1,16 @@
+import calendar
+from datetime import date
+import holidays
+import config
+
+
+def get_this_month():
+    month = date.today().month
+    year = date.today().year
+
+    ua_holidays = holidays.country_holidays(config.country)
+    for i in range(1, calendar.monthrange(year, month)[1] + 1):
+        day = date(year, month, i)
+        weekday = day.weekday()
+        if weekday in range(0, 5) and day not in ua_holidays:
+            yield day.strftime("%Y-%m-%d"), weekday
