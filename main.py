4f269992f0813ccf254c9a02227a4cc3f54038b6
Olexandr Deviatov <sasca9719@gmail.com>
2022-08-06 11:56:17 +0300

    added rally entities and refactoring

--- main.py
+++ main.py
@@ -1,44 +1,25 @@
-import requests
-from toggl.TogglPy import Toggl
-
-import config
-from rally import rally_rest
-from toggle import time_entities, toggle_rest
-from utils.dates import get_this_month
-
-
-def create_new_time_entities(is_daily, is_english):
-    toggl = Toggl()
-    toggl.setAuthCredentials(config.username_toggle, config.password_toggle)
-
-    for date, weekday in get_this_month():
-        if weekday in range(0, 4) and is_daily:
-            functor = time_entities.create_daily
-        elif weekday == 4 and is_english:
-            functor = time_entities.create_english
-        else:
-            raise Exception("Wrong weekday")
-
-        data = functor(date)
-        url = toggle_rest.toggle_host + toggle_rest.toggle_time_entities_route
-        toggl.postRequest(url, parameters=data)
-
-
-def query_rally_tasks():
-    query = f'((CreatedBy = {config.username_rally}) AND \
-                ((LastUpdateDate >= 2022-06-01) AND (LastUpdateDate <= today)))'
-    query = '?query=' + query
-
-    url = rally_rest.host + rally_rest.rally_artifacts_route + query
-    return requests.get(url, headers={'ZSESSIONID': config.api_key_rally})
+from rally.rally_data import query_rally_tasks
+from toggle import time_entities, toggle_sender
+from utils.dates import get_this_month_range
 
 
 if __name__ == '__main__':
     is_daily = False
     is_english = False
-    is_rally = False
+    is_rally = True
 
-    create_new_time_entities(is_daily, is_english)
+    this_month = list(get_this_month_range())
     if is_rally:
-        response = query_rally_tasks()
-        print(response.text)
+        tasks = list(query_rally_tasks(this_month[0][0]))
+        tasks.sort(key=lambda el: el.start_at)
+        print(tasks)
+
+    toggle_sender = toggle_sender.ToggleSender()
+
+    for date, weekday in this_month:
+        if weekday in range(0, 4) and is_daily:
+            toggle_sender.send(time_entities.create_daily(date))
+        elif weekday == 4 and is_english:
+            toggle_sender.send(time_entities.create_english(date))
+        else:
+            continue
--- toggle/time_entities.py
+++ toggle/time_entities.py
@@ -1,11 +1,17 @@
+iSID_id = 175371695
+internal_id = 176403548
+fifteen_min = 900
+hour = 4 * fifteen_min
+
+
 def create_daily(date):
     return {
         "time_entry":
             {
                 "description": "Daily meeting",
                 "start": f"{date}T10:30:00+03:00",
-                "duration": 900,
-                "pid": 175371695,
+                "duration": fifteen_min,
+                "pid": iSID_id,
                 "created_with": "API"
             }
     }
@@ -17,8 +23,47 @@ def create_english(date):
             {
                 "description": "Lesson",
                 "start": f"{date}T15:00:00+03:00",
-                "duration": 3600,
-                "pid": 176403548,
+                "duration": hour,
+                "pid": internal_id,
+                "created_with": "API"
+            }
+    }
+
+
+def create_rally(date, name):
+    return {
+        "time_entry":
+            {
+                "description": f"{name}",
+                "start": f"{date}T10:45:00+03:00",
+                "duration": 7 * hour + 3 * fifteen_min,
+                "pid": iSID_id,
+                "created_with": "API"
+            }
+    }
+
+
+def create_rally_friday_before(date, name):
+    return {
+        "time_entry":
+            {
+                "description": f"{name}",
+                "start": f"{date}T10:00:00+03:00",
+                "duration": 4 * hour,
+                "pid": iSID_id,
+                "created_with": "API"
+            }
+    }
+
+
+def create_rally_friday_after(date, name):
+    return {
+        "time_entry":
+            {
+                "description": f"{name}",
+                "start": f"{date}T16:00:00+03:00",
+                "duration": 3 * hour,
+                "pid": iSID_id,
                 "created_with": "API"
             }
     }
