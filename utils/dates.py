fc114d76af6883047a0dbf24f0933c9ca6b7eddc
Olexandr Deviatov <sasca9719@gmail.com>
2022-08-29 21:47:01 +0300

    added vacation and boundaries to month days

--- utils/dates.py
+++ utils/dates.py
@@ -4,13 +4,18 @@ import holidays
 import config
 
 
-def get_this_month_range():
+def get_this_month_range(day_from):
     month = date.today().month
     year = date.today().year
+    current_day = date.today().day
 
     ua_holidays = holidays.country_holidays(config.country)
-    for i in range(1, calendar.monthrange(year, month)[1] + 1):
+    work_week = range(0, 5)
+    last_day = calendar.monthrange(year, month)[1]
+    last_day = current_day if last_day > current_day else last_day
+
+    for i in range(day_from, last_day + 1):
         day = date(year, month, i)
         weekday = day.weekday()
-        if weekday in range(0, 5) and day not in ua_holidays:
+        if weekday in work_week and day not in ua_holidays and i not in config.vacation:
             yield day.strftime("%Y-%m-%d"), weekday
